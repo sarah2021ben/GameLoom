8be93ae720f4633ccf44fefa16ffd2c692c6fb17
sarah-casbatech <sarah.benkanoun@factorydigitale.tech>
2023-11-09 14:52:02 +0100

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button } from '@chakra-ui/react';
-function App() {
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-  return <Button colorScheme='blue'>Button</Button>
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav"
+            "aside main"`,
+      }}
+    >
+      <GridItem bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="blue.300" area={"aside"}>
+          aside
+        </GridItem>
+      </Show>
+
+      <GridItem bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
