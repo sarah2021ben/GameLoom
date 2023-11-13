17cd4ed926ba2445b763f8d4f620353451119168
sarah-casbatech <sarah.benkanoun@factorydigitale.tech>
2023-11-13 12:10:40 +0100

    displaying game score

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,18 +1,32 @@
 import { Card, CardBody } from "@chakra-ui/card";
 import { Game } from "../Hooks/useGame";
-import { Heading, Image, Text } from "@chakra-ui/react";
+import { Badge, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
+  let color =
+    game.metacritic > 75 ? "green" : game.metacritic > 60 ? "yellow" : "";
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p=> p.platform)} />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <Badge
+            colorScheme={color}
+            fontSize="14px"
+            paddingX={2}
+            borderRadius={"5px"}
+          >
+            {game.metacritic}
+          </Badge>
+        </HStack>
       </CardBody>
     </Card>
   );
