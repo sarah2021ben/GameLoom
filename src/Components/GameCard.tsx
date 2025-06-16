7d90d8003c590bb4e40e48265f33bfb7522c326d
sarah_ben <benkanoun.sarah@gmail.com>
2025-06-16 15:13:57 +0100

    trancate game title

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -16,7 +16,7 @@ const GameCard = ({ game }: Props) => {
       <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <Heading fontSize={"2xl"} isTruncated>{game.name}</Heading>
           <HStack justifyContent="space-between">
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
